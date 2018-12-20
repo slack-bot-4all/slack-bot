2d8c35b1eacf50253f23afdad4a6e0c381b05c8b
Cayo Hollanda <cayohollanda@hotmail.com>
2018-12-20 09:45:32 -0500

    agr sim

--- rancher.go
+++ rancher.go
@@ -171,11 +171,11 @@ func (ranchListener *RancherListener) UpdateCustomHaproxyCfg(ID string, newPerce
 			if firstWeight == "" {
 				firstWeight = line[1]
 				atual1 = line[1]
-				newLbConfig = strings.Replace(actualLbConfig, fmt.Sprintf("weight %s", firstWeight), fmt.Sprintf("weight %s", newPercent), 1)
+				//newLbConfig = strings.Replace(actualLbConfig, fmt.Sprintf("weight %s", firstWeight), fmt.Sprintf("weight %s", newPercent), 1)
 				r.Replace(firstWeight)
 			} else {
 				secondWeight = line[1]
-				newLbConfig = strings.Replace(newLbConfig, fmt.Sprintf("weight %s", secondWeight), fmt.Sprintf("weight %s", oldPercent), 1)
+				//newLbConfig = strings.Replace(newLbConfig, fmt.Sprintf("weight %s", secondWeight), fmt.Sprintf("weight %s", oldPercent), 1)
 				atual2 = line[1]
 				r.Replace(secondWeight)
 			}
