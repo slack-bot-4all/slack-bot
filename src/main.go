5b46dca35c3b1f7db151d65888ca67dbcd800d13
Cayo Hollanda <cayohollanda@hotmail.com>
2019-04-27 18:41:07 -0300

    adding logic to create admin user if not exists on database

--- src/main.go
+++ src/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/slack-bot-4all/slack-bot/src/repository"
+
 	"github.com/slack-bot-4all/slack-bot/src/model"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -39,6 +41,15 @@ func initializeDB() error {
 	log.Println("[INFO] Connected to database")
 
 	config.DB.AutoMigrate(&model.Rancher{}, &model.User{}, &model.Task{})
+
+	adminUser := model.User{
+		Username: "admin",
+		Password: "admin",
+	}
+	err = repository.FindUserByUsername(&adminUser)
+	if err != nil {
+		repository.AddUser(&adminUser)
+	}
 
 	return nil
 }
